92f845cd49d61f9ea17468ed079f9773bf46ac87
Marcos Silvano <marcossilvano@gmail.com>
2020-11-21 18:50:07 -0300

    digite uma nota exercicio 12-11-2020

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace pedir_nota_de_1_a_10_18_11_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Obs:De 0 a 10.");
+            Console.Write("Digite a nota: ");
+            int nota = int.Parse(Console.ReadLine());
+
+            while(nota > 10){
+                Console.WriteLine("A nota não é válida");
+                Console.Write("Digite o valor novamente: ");
+                nota = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Nota cadastrada!");
+        }
+    }
+}
